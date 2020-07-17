54d5b569c673a6d699e3e33865c7a15d93b3ab64
Eamonn de Leastar <edeleastar@wit.ie>
2020-07-17 11:55:59 +0100

    au 2 leaflet demo

---
+++ src/my-app.ts
@@ -0,0 +1,19 @@
+import { LeafletMap } from "services/leaflet-map";
+
+export class MyApp {
+  public message = 'Hello World!';
+  mapDescriptor = {
+    id: "home-map-id",
+    height: 1200,
+    location: { lat: 53.2734, long: -7.7783203 },
+    zoom: 8,
+    minZoom: 7,
+    activeLayer: "",
+  };
+
+  map: LeafletMap;
+
+  afterAttach() {
+    this.map = new LeafletMap(this.mapDescriptor);
+  }
+}
